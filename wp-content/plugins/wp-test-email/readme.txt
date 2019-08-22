540324af9769b7847511765e2f7f3150359db9c0
Calin Don <calin.don@gmail.com>
2019-08-22 16:18:37 +0300

    Add WP Test Mail plugin

---
+++ wp-content/plugins/wp-test-email/readme.txt
@@ -0,0 +1,33 @@
+=== WP Test Email ===
+Contributors: boopathi0001
+Donate link: https://paypal.me/boopathirajan
+Tags: Test email, Check Mail, Mail Tester, WP Mail, wp_email, Test WordPress Mail, Check WordPress Mail, PHP Mail
+Requires at least: 4.3
+Tested up to: 5.2.2
+Requires PHP: 5.2.4
+Stable tag: 1.1.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+WP Test Email is allows you to test if your WordPress installation is sending mail or not.
+
+== Description ==
+
+Do you want to test whether the WordPress installation is sending mail or not?. WP Test Email is helps you to do that.
+It allows you to send simple test email to an email address of your choice.
+
+Kindly let us know your feedback or comments to add more features in this plugin.
+
+== Installation ==
+
+1. Log in to your WordPress admin panel and go to Plugins -> Add New
+2. Type **WP Test Email** in the search box and click on search button.
+3. Find WP Test Email plugin.
+4. Then click on Install Now after that activate the plugin.
+
+OR
+
+1. Download and save the **WP Test Email** plugin to your hard disk.
+2. Login to your WordPress and go to the Add Plugins page.
+3. Click Upload Plugin button to upload the zip.
+4. Click Install Now to install and activate the plugin.
---
+++ wp-content/plugins/wp-test-email/wp-test-email.php
@@ -0,0 +1,79 @@
+<?php
+/*
+Plugin Name: WP Test Email
+description: WP Test Email is allows you to test if your WordPress installation is sending mail or not.
+Version: 1.1.3
+Author: Boopathi Rajan
+Text Domain: wp-test-email
+Author URI: https://www.boopathirajan.com
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+function register_wp_test_email_page() {
+	add_submenu_page( 'tools.php', "Test Email", "Test Email", 'manage_options', 'wp-test-email', 'wp_test_email' );
+}
+add_action('admin_menu', 'register_wp_test_email_page');
+
+function wp_test_email() 
+{	
+?>
+<div class="wrap">
+	<h1><?php _e( 'Test Mail', 'wp-test-email' ); ?></h1>
+	<form method="post">		
+		<?php
+		if(isset($_POST['mail_to']))
+		{
+			 
+			 if(wp_verify_nonce($_POST['wp_test_email_nonce_field'], 'wp_test_email_nonce_action'))
+			 {
+				if(!empty($_POST['mail_to']))
+				{
+					$to=sanitize_email($_POST['mail_to']);
+					$subject=sanitize_text_field($_POST['mail_subject']);
+					$body=" This is the test mail from ".get_bloginfo('name');
+					$headers = array('Content-Type: text/html; charset=UTF-8'); 
+					$test_email=wp_mail( $to, $subject, $body );
+					if($test_email)
+					{
+						?>
+						<div class="notice notice-success is-dismissible">
+							<p><?php _e( 'Email has been sent!', 'wp-test-email' ); ?></p>
+						</div>
+						<?php
+					}
+					else
+					{
+						?>
+						<div class="notice notice-error is-dismissible">
+							<p><?php _e( 'Email not sent!', 'wp-test-email' ); ?></p>
+						</div>
+						<?php
+					}
+				}
+			 }
+		}
+		?>
+		<table class="form-table">
+			<tr valign="top">
+				<th scope="row"><?php _e( 'To', 'wp-test-email' ); ?></th>
+				<td>
+					<input type="email" name="mail_to" value=""/>
+					<p class="description"><i><?php _e( 'Enter "To address" here.', 'wp-test-email' ); ?></i></p>
+				</td>
+			</tr> 
+			<tr valign="top">
+				<th scope="row"><?php _e( 'Subject', 'wp-test-email' ); ?></th>
+				<td>
+					<input type="text" name="mail_subject" value="Test Mail"/>
+					<p class="description"><i><?php _e( 'Enter mail subject here', 'wp-test-email' ); ?></i></p> 
+				</td>
+			</tr> 			
+		</table>    
+		<?php wp_nonce_field( 'wp_test_email_nonce_action', 'wp_test_email_nonce_field' ); ?>
+		<?php submit_button(); ?>
+	</form>
+</div>
+<?php 
+}
+?>
