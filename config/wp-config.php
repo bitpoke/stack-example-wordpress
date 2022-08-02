255f4f682e6a8c692e0f9dd42f88a05fb7ea5227
Calin Don <calin@bitpoke.cloud>
2022-08-02 22:57:16 +0300

    Add `.devcontainer.json` for Github Codespaces

--- config/wp-config.php
+++ config/wp-config.php
@@ -1,5 +1,9 @@
 <?php
 // Set custom wp-config.php settings in this file 
+if ( 'true' === getenv('CODESPACES') ) {
+	// on Github Codespaces obey the X-Forwarded-Host header
+	$_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_FORWARDED_HOST'];
+}
 
 if ( 'development' === getenv('WP_ENV') ) {
 	define( 'DISALLOW_FILE_EDIT', false );
